c47abc0b8f8813d5d5599b5ff4b516cd869d1ee3
Sebastian Castro <90scastro@gmail.com>
2017-06-22 17:01:14 +0200

    Adds hash getter for element route

--- src/js/gogocarto.ts
+++ src/js/gogocarto.ts
@@ -34,6 +34,12 @@ export class GoGoCartoModule
 		this.app.loginModule.setRole($role);
 	}
 
+	// return the given hash to add to url so gogocarto app will open on specific element
+	getElementRouteHash($elementId, $elementName = 'find')
+	{
+		return this.app.routerModule.generate('biopen_directory_showElement', { id: $elementId, name: $elementName });
+	}
+
 	private checkForDistantConfifuration(options : string|any)
 	{
 		if ( typeof options === 'object')
