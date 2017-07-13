b755274d7e701cc3219472c6f3a0e2d89e144ed8
Sebastian Castro <90scastro@gmail.com>
2017-07-13 10:47:42 +0200

    Delete unused functions

--- src/js/components/element-menu.component.ts
+++ src/js/components/element-menu.component.ts
@@ -52,27 +52,6 @@ function handleDirectionsPickingAddress()
 	}
 }
 
-function deleteElement()
-{
-	if (grecaptcha.getResponse().length === 0)
-	{
-		$('#captcha-error-message').show();
-		grecaptcha.reset();
-	}
-	else
-	{
-		$('#captcha-error-message').hide();
-		$('#popup-report-error').closeModal();
-	}	
-}
-
-function onloadCaptcha() 
-{
-    grecaptcha.render('captcha', {
-      'sitekey' : '6LcEViUTAAAAAOEMpFCyLHwPG1vJqExuyD4n1Lbw'
-    });
-}
-
 export function updateFavoriteIcon(object, element : Element)
 {
 	if (!element.isFavorite) 
@@ -188,16 +167,3 @@ export function getCurrentElementInfoBarShown()
 	}
 	return $('.element-item.active');
 }
-
-
-/*function bookMarkMe()
-{
-	if (window.sidebar) { // Mozilla Firefox Bookmark
-      window.sidebar.addPanel(location.href,document.title,"");
-    } else if(window.external) { // IE Favorite
-      window.external.AddFavorite(location.href,document.title); }
-    else if(window.opera && window.print) { // Opera Hotlist
-      this.title=document.title;
-      return true;
-    }
-}*/
