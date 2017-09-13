34a020ab8ab6cf7b06f081f18e42be8cfcbb42d1
Sebastian Castro <90scastro@gmail.com>
2017-09-13 10:49:17 +0200

    Adds singleclick event leaflet plugin

--- src/js/libs/leaflet.markercluster-src.js
+++ src/js/libs/leaflet.markercluster-src.js
@@ -3,10 +3,51 @@
  https://github.com/Leaflet/Leaflet.markercluster
  (c) 2012-2013, Dave Leaver, smartrak
 */
-(function (window, document, undefined) {/*
+(function (window, document, undefined) {
+
+/* single click events */
+L.Evented.addInitHook( function () {
+    this._singleClickTimeout = null;
+    this.on( 'click', this._scheduleSingleClick, this );
+    this.on( 'dblclick dragstart zoomstart', this._cancelSingleClick, this );
+});
+
+L.Evented.include({
+
+    _cancelSingleClick : function(){
+        // This timeout is key to workaround an issue where double-click events
+        // are fired in this order on some touch browsers: ['click', 'dblclick', 'click']
+        // instead of ['click', 'click', 'dblclick']
+        setTimeout( this._clearSingleClickTimeout.bind(this), 0 );
+    },
+
+    _scheduleSingleClick: function(e) {
+        this._clearSingleClickTimeout();
+
+        this._singleClickTimeout = setTimeout(
+            this._fireSingleClick.bind(this, e),
+            (this.options.singleClickTimeout || 250)
+        );
+    },
+
+    _fireSingleClick: function(e){
+        if ( !e.originalEvent._stopped ) {
+            this.fire( 'singleclick', L.Util.extend( e, { type : 'singleclick' } ) );
+        }
+    },
+
+    _clearSingleClickTimeout: function(){
+        if (this._singleClickTimeout != null) {
+            clearTimeout( this._singleClickTimeout );
+            this._singleClickTimeout = null;
+        }
+    }
+
+});
+
+/*
  * L.MarkerClusterGroup extends L.FeatureGroup by clustering the markers contained within
  */
-
 L.MarkerClusterGroup = L.FeatureGroup.extend({
 
 	options: {
