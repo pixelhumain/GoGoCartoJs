0c2893cf98923c8982cdb0700c43a25a40025a39
Sebastian Castro <90scastro@gmail.com>
2017-12-20 11:41:06 +0100

    Update header file mesage (name and license)

--- src/js/modules/display-element-alone.module.ts
+++ src/js/modules/display-element-alone.module.ts
@@ -1,10 +1,10 @@
 /**
- * This file is part of the MonVoisinFaitDuBio project.
+ * This file is part of the GoGoCarto project.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * @copyright Copyright (c) 2016 Sebastian Castro - 90scastro@gmail.com
- * @license    MIT License
+ * @license GNU GPL v3
  * @Last Modified time: 2016-12-13
  */
 
