ab4a780708192e7afe6a6259793571796989967f
Sebastian Castro <90scastro@gmail.com>
2018-06-12 15:48:40 +0200

    Fixs dev data

--- web/data/elements-single.js
+++ web/data/elements-single.js
@@ -9,7 +9,7 @@ var elements = [
       "longitude":-0.0339
     },
 
-    "optionValues": [ "10427", 'Market' ],
+    "categories": [ 10427 ],
 
     "address": {
       "streetAddress":"5 rue Edmond Proust",
@@ -23,13 +23,25 @@ var elements = [
     "descriptionMore": "Un rassemblement était organisé à 13 heures à Paris sous les fenêtres de la ministre de la Santé Agnès Buzyn aux Invalides. Le cortège est ensuite parti de Montparnasse vers 14 heures. Notre reporter Mona Hammoud est sur place. Des représentants de forces politiques étaient présents pour exprimer leur solidarité avec les retraités. La conseillère de Paris, Danielle Simonnet et les députés Adrien Quatennens et Clémentine Autain (La France insoumise) étaient dans le cortège.",
     "email": "example@gogocarto.fr",
     "telephone": "055452545",
-    "website": "https://example.fr",
-    "image" : "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.italyguides.it%2Fimages%2Fgridfolio%2Ftaormina%2Ftaormina.02.jpg&f=1",
+    
+    // "image" : "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.italyguides.it%2Fimages%2Fgridfolio%2Ftaormina%2Ftaormina.02.jpg&f=1",
+    "images": ["http://engineering.naukri.com/wp-content/uploads/sites/19/2015/10/open-source.png", "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.clipartbest.com%2Fcliparts%2FRcA%2FErG%2FRcAErGEei.png&f=1"],
 
+    "website": "https://example.fr",
+    "urls": {
+      "communecter": "http://communeter.org",
+      "peertube": "http://framatub.org"
+    },
+    // "urls": ["http://communeter.org", "http://framatube.org"],
+    // "urls": [
+    //   {"type": "communecter", "value": "http://communeter.org" },
+    //   {"type": "peertube", "value": "http://framatube.org" }
+    // ],
+    // "url" : "http://communeter.org",
     "openHours": {
       "Mo":"09:00-12:00",
       "Fr":"09:00-11:30 & 5pm to 9pm"
-    },
-    "openHoursString": "Fermé pendant les vacances d'été",
+    },  
+    "openHoursString": "Fermé pendant les vacances d'été"
   }
 ]
