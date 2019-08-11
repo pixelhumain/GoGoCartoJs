c5e03c942949e257ecdaf6fdd78e444b0dc251f5
regazzoj <jeanbaptiste.regazzoni@protonmail.com>
2019-08-11 23:09:26 +0200

    Last task #20 Internationalisation
    Add a script to look for missing entries in the different 'locales' directory files (english locale file is use as a default one, its entries are the default ones)
    
    _______________________________________
    
    Ajout d'un script pour rechercher les clés manquantes dans les différents fichiers du dossier 'locales' (le fichier anglais est utilisé par défaut, ses clés sont les clés par défaut)

---
+++ addMissingEntries.js
@@ -0,0 +1,99 @@
+var fs = require('fs');
+
+function browseLocalesDirectory()
+{
+  return fs.readdirSync('src/locales');
+}
+
+function getLocalesFiles(directoryFiles)
+{
+  // Get content of every file of the 'locales' directory
+  let translationsFiles = [];
+
+  for(let i=0; i<directoryFiles.length; ++i)
+  {
+    translationsFiles.push(fs.readFileSync('src/locales/'+directoryFiles[i], {  encoding: 'utf8'  }));
+    if(directoryFiles[i]=='en.ts')
+    {
+      translationsFiles.unshift(translationsFiles.pop());
+      directoryFiles.unshift(directoryFiles.splice(i,1)[0]);
+    }
+  }
+
+  return translationsFiles;
+}
+
+function removeComments(content)
+{
+  // Remove comments
+  let currentCharacter = 'A'.charCodeAt();
+  for(let index=0;index<26; ++index)
+  {
+    content = content.replace("//"+String.fromCharCode(currentCharacter++),"");
+  }
+
+  return content;
+}
+
+function checkEntries(files)
+{
+  for(let i=1;i<files.length;++i)
+  {
+    for(let currentEntry in files[0])
+    {
+      if(!files[i][currentEntry])
+      {
+        files[i][currentEntry] = '';
+      }
+    }
+  }
+}
+
+function beautifyAndWriteFile(JSONContent, beforeEqualText, filepath)
+{
+  let content = JSON.stringify(JSONContent);
+  content = beforeEqualText + '\n' + content;
+  content = content.replace('{','{  ');
+  content = content.replace(/","/gi,'",\n  "');
+  content = content.replace('}','\n}');
+
+  let currentCharacter = 'a'.charCodeAt(),
+    currentUpperCaseCharacter = 'A'.charCodeAt();
+  for(let index=0; index<26; ++index)
+  {
+    content = content.replace('  "'+String.fromCharCode(currentCharacter),'\n  //'+String.fromCharCode(currentUpperCaseCharacter++)+'\n  "'+String.fromCharCode(currentCharacter++))
+  }
+
+  fs.writeFileSync(filepath, content);
+}
+
+function addMissingEntries()
+{
+  let directoryFiles = browseLocalesDirectory();
+
+  let translationsFiles = getLocalesFiles(directoryFiles);
+
+  let translationsFilesJSON = [],
+    beforeEqualTexts = [];
+
+  for(let index=0; index<translationsFiles.length; ++index)
+  {
+    let equalPosition = translationsFiles[index].indexOf('=');
+    beforeEqualTexts.push(translationsFiles[index].substring(0, equalPosition+1));
+    translationsFilesJSON[index] = JSON.parse(removeComments(translationsFiles[index].substring(equalPosition+1)));
+  }
+
+  checkEntries(translationsFilesJSON);
+
+  // No need to write the 'en.ts' file because it is our default file
+  translationsFilesJSON.shift();
+  beforeEqualTexts.shift();
+  directoryFiles.shift();
+
+  for(let index=0; index<translationsFilesJSON.length; ++index)
+  {
+    beautifyAndWriteFile(translationsFilesJSON[index], beforeEqualTexts[index], 'src/locales/'+directoryFiles[index]);
+  }
+}
+
+module.exports = addMissingEntries;
